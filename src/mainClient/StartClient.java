6851fa919df5367f7a75e9414173bda66857124b
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2021-03-24 17:19:49 +0100

    Adding Print of Current Version on start

--- src/mainClient/StartClient.java
+++ src/mainClient/StartClient.java
@@ -6,6 +6,10 @@ public class StartClient {
 
     public static void main(String[] args) {
 
+        String version = "V.0.4.0";
+
+        System.out.println("Starte Ver. " + version);
+
         Scanner inputscan = new Scanner(System.in);
         System.out.println("Input Hostname");
         String hostname = inputscan.nextLine();
